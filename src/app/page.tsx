800ee716421d2244b3444960f2ea39edea12999f
Chua Siang Poh <siangpoh@outlook.com>
2025-09-03 15:34:11 +0000

    fix page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import { List, Search } from "@deemlol/next-icons";
-
 export default function Page() {
-  return (
-    <div className="">
-      <a className="flex flex-row flex-nowrap items-center-safe" href="locations?filterBy=nearest">
-        <Search />
-        <span className="text-nowrap">Quick search</span>
-      </a>
-      <a className="flex flex-row flex-nowrap items-center-safe" href="locations">
-        <List />
-        <span className="text-nowrap">Browser</span>
-      </a>
-    </div>
-  );
+  return null;
 }
