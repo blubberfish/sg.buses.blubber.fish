1ed36f3e6fdd93acdf04cd08f3537183d7ddf9e1
Chua Siang Poh <siangpoh@outlook.com>
2025-11-16 09:23:49 +0000

    remove obsolete

--- src/app/_components/nav/group.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function NavGroup({ children }: React.PropsWithChildren) {
-  return <nav className="flex flex-row flex-no-wrap border-b border-b-neutral-300">{children}</nav>;
-}
