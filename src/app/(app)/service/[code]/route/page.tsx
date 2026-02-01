55dba45a4651546350bf22d6fc88b919503334a2
Chua Siang Poh <siangpoh@outlook.com>
2026-02-01 15:22:58 +0000

    fix lint and remove obsolete

--- src/app/(app)/service/[code]/route/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function Page({ params }: { params: Promise<{ code: string }> }) {
-  const { code } = await params;
-  return <div>Service Code: {code}</div>;
-}
--- src/lib/components/dashboard/container.tsx
+++
@@ -1,5 +0,0 @@
-import { HTMLAttributes } from "react";
-
-export function DashboardContainer(props: HTMLAttributes<HTMLDivElement>) {
-  return <div className="w-screen min-h-svh" {...props} />;
-}
